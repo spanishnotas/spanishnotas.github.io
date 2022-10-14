32793f3ca4891a2007bdb58fcd45b2d26f352d11
vinay <vinay.lakkam@gmail.com>
2022-10-15 04:18:15 +0530

    sidebar

--- js/scripts.js
+++ js/scripts.js
@@ -8,19 +8,35 @@
 // 
 
 window.addEventListener('DOMContentLoaded', event => {
-
+    alert('dom loaded')
     // Toggle the side navigation
     const sidebarToggle = document.body.querySelector('#sidebarToggle');
     if (sidebarToggle) {
+        document.body.classList.toggle('sb-sidenav-toggled');
         // Uncomment Below to persist sidebar toggle between refreshes
         // if (localStorage.getItem('sb|sidebar-toggle') === 'true') {
         //     document.body.classList.toggle('sb-sidenav-toggled');
         // }
         sidebarToggle.addEventListener('click', event => {
             event.preventDefault();
+            //document.body.scrollTop = document.documentElement.scrollTop = 0;
             document.body.classList.toggle('sb-sidenav-toggled');
-            localStorage.setItem('sb|sidebar-toggle', document.body.classList.contains('sb-sidenav-toggled'));
+            //localStorage.setItem('sb|sidebar-toggle', document.body.classList.contains('sb-sidenav-toggled'));
         });
     }
 
+
+    document.querySelectorAll('#sidebar-wrapper a')
+        .forEach(link => link.addEventListener('click', event => {
+                    alert('selected')
+                    document.body.classList.toggle('sb-sidenav-toggled');
+    }));
+
 });
+
+function toggleSidebar(event, id) {
+//alert('toggleSidebar' + event);
+//event.preventDefault();
+//document.body.classList.toggle('sb-sidenav-toggled');
+//document.getElementById(id).scrollIntoView();
+}
